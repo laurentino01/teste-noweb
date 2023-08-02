94d60ca611579aa5ae1e8dfa0d630310ed8e77fd
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-02 16:08:16 -0300

    setting categories

--- src/page/home/categories/Categories.tsx
+++ src/page/home/categories/Categories.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 import { Typography } from "../../../components/typography/Typography";
+import { ShowCase } from "../../../components/showcase/ShowCase";
 import pattern from "../../../assets/whitepattern.svg";
+import lampada from "../../../assets/products-imgs/lampada.svg";
+
 import "./categories.css";
+
+interface ICategoriesData {
+  children: string;
+  srcImg: string;
+}
+
 export const Categories = () => {
+  const [categoriestList, setCategoriesList] = useState<ICategoriesData[]>([]);
+
+  useEffect(() => {
+    const list = [
+      {
+        children: "LÂMPADAS",
+        srcImg: lampada,
+      },
+    ];
+
+    setCategoriesList(list);
+  }, []);
+
   return (
     <section className="categories">
       <div className="categories-subtitle-area">
@@ -11,6 +35,13 @@ export const Categories = () => {
             CATEGORIAS
           </Typography>
         </div>
+      </div>
+      <div className="categories-categories-area">
+        {categoriestList.map((item) => (
+          <ShowCase key={item.srcImg} srcImg={item.srcImg} size="normal">
+            {item.children}
+          </ShowCase>
+        ))}
       </div>
     </section>
   );
