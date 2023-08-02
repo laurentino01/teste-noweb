84c9451d7be8e18b6e5ab95f29ffb8660bd92541
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-02 08:02:08 -0300

    creating products

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "./hero/hero";
+import { Products } from "./products/Products";
 
 export const Home = () => {
   return (
     <div>
       <Hero />
+      <Products />
     </div>
   );
 };
---
+++ src/page/home/products/Products.tsx
@@ -0,0 +1,44 @@
+import productBG from "../../../assets/bg-imgs/products-background.svg";
+import { Card } from "../../../components/cards/Card";
+import { Typography } from "../../../components/typography/Typography";
+import lustres from "../../../assets/products-imgs/lustre.svg";
+import "./Products.css";
+
+export const Products = () => {
+  return (
+    <section className="products">
+      <div className="title-area">
+        <img src={productBG} alt="" />
+        <Typography variant="subtitle" element="h2">
+          PRODUTOS EM DESTAQUE
+        </Typography>
+      </div>
+      <div className="products-area">
+        <Card
+          srcImg={lustres}
+          tag="Lustres"
+          title="Lustre suspenso rústico"
+          oldPrice="R$ 1.500,00"
+          newPrice="R$ 1.000,00"
+          paymentDatail="ou em 3x de R$ 333,33"
+        />
+        <Card
+          srcImg={lustres}
+          tag="Lustres"
+          title="Lustre suspenso rústico"
+          oldPrice="R$ 1.500,00"
+          newPrice="R$ 1.000,00"
+          paymentDatail="ou em 3x de R$ 333,33"
+        />
+        <Card
+          srcImg={lustres}
+          tag="Lustres"
+          title="Lustre suspenso rústico"
+          oldPrice="R$ 1.500,00"
+          newPrice="R$ 1.000,00"
+          paymentDatail="ou em 3x de R$ 333,33"
+        />
+      </div>
+    </section>
+  );
+};
