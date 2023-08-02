ba719111c8a7459120dd9f6fed2e8310b4e10ed8
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-02 08:29:39 -0300

    feat(product) almost complet

--- src/page/home/products/Products.tsx
+++ src/page/home/products/Products.tsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
+
 import productBG from "../../../assets/bg-imgs/products-background.svg";
 import { Card } from "../../../components/cards/Card";
 import { Typography } from "../../../components/typography/Typography";
 import lustres from "../../../assets/products-imgs/lustre.svg";
+
 import "./Products.css";
-import { useEffect } from "react";
+
+interface IProductListData {
+  srcImg: string;
+  tag: string;
+  title: string;
+  oldPrice: string;
+  newPrice: string;
+  paymentDatail: string;
+}
 
 export const Products = () => {
-  useEffect(() => {}, []);
+  const [productList, setProductList] = useState<IProductListData[]>([]);
+
+  useEffect(() => {
+    const list = [
+      {
+        srcImg: lustres,
+        tag: "Lustres",
+        title: "Lustre suspenso rústico",
+        oldPrice: "R$ 1.500,00",
+        newPrice: "R$ 1.000,00",
+        paymentDatail: "ou em 3x de R$ 333,33",
+      },
+      {
+        srcImg: "lampada",
+        tag: "Lâmpadas",
+        title: "Lâmpada",
+        oldPrice: "R$ 60,00",
+        newPrice: "R$ 45,00",
+        paymentDatail: "ou em 2x de R$ 22,50",
+      },
+    ];
+
+    setProductList(list);
+  }, []);
 
   return (
     <section className="products">
@@ -17,46 +51,17 @@ export const Products = () => {
         </Typography>
       </div>
       <div className="products-area">
-        <Card
-          srcImg={lustres}
-          tag="Lustres"
-          title="Lustre suspenso rústico"
-          oldPrice="R$ 1.500,00"
-          newPrice="R$ 1.000,00"
-          paymentDatail="ou em 3x de R$ 333,33"
-        />
-        <Card
-          srcImg={lustres}
-          tag="Lustres"
-          title="Lustre suspenso rústico"
-          oldPrice="R$ 1.500,00"
-          newPrice="R$ 1.000,00"
-          paymentDatail="ou em 3x de R$ 333,33"
-        />
-        <Card
-          srcImg={lustres}
-          tag="Lustres"
-          title="Lustre suspenso rústico"
-          oldPrice="R$ 1.500,00"
-          newPrice="R$ 1.000,00"
-          paymentDatail="ou em 3x de R$ 333,33"
-        />
-        <Card
-          srcImg={lustres}
-          tag="Lustres"
-          title="Lustre suspenso rústico"
-          oldPrice="R$ 1.500,00"
-          newPrice="R$ 1.000,00"
-          paymentDatail="ou em 3x de R$ 333,33"
-        />
-        <Card
-          srcImg={lustres}
-          tag="Lustres"
-          title="Lustre suspenso rústico"
-          oldPrice="R$ 1.500,00"
-          newPrice="R$ 1.000,00"
-          paymentDatail="ou em 3x de R$ 333,33"
-        />
+        {productList.map((item) => (
+          <Card
+            key={item.title}
+            srcImg={item.srcImg}
+            tag={item.tag}
+            title={item.title}
+            oldPrice={item.oldPrice}
+            newPrice={item.newPrice}
+            paymentDatail={item.paymentDatail}
+          />
+        ))}
       </div>
     </section>
   );
