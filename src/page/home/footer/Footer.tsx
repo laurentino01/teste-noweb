031e8d43b5f0c7a01a0b6745fc66894976bebeef
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-03 08:07:17 -0300

    footer init

---
+++ src/page/home/footer/Footer.tsx
@@ -0,0 +1,35 @@
+import { Typography } from "../../../components/typography/Typography";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <Typography element="p" variant="paragraph">
+            Venha na Elétrica J. Santos e garanta a melhor opção para sua casa!
+          </Typography>
+        </div>
+        <div>
+          <Typography element="p" variant="paragraph">
+            NOS SIGA NAS REDES
+          </Typography>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Typography element="p" variant="paragraph">
+            &copy Copyright 2021 - Elétrica J. Santos - Todos os Direitos
+            Reservados
+          </Typography>
+        </div>
+        <img src="" alt="" />
+      </div>
+    </footer>
+  );
+};
