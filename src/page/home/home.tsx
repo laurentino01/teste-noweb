333c3450481ae4a97086b66689b35ad223baa479
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-03 08:43:48 -0300

    version 1.0.0

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -5,6 +5,7 @@ import { Hero } from "./hero/hero";
 import { Products } from "./products/Products";
 import { Telesales } from "./telesales/Telesales";
 
+//version 1.0.0
 export const Home = () => {
   return (
     <div>
