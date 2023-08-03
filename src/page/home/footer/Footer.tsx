8a4dc4fad9ae9ea41d4e0da2f6c5844754abbd66
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-03 08:42:47 -0300

    feat(footer) responsive

--- src/page/home/footer/Footer.tsx
+++ src/page/home/footer/Footer.tsx
@@ -1,10 +1,16 @@
 import { Typography } from "../../../components/typography/Typography";
 import logo from "../../../assets/logo-white.svg";
+import whats from "../../../assets/icons/whatsapp-icon.svg";
+import insta from "../../../assets/icons/instagram-icon.svg";
+import face from "../../../assets/icons/facebook-icon.svg";
+import logoNoWeb from "../../../assets/logo-noweb.svg";
+import pattern from "../../../assets/footer-pattern.svg";
 import "./footer.css";
 
 export const Footer = () => {
   return (
     <footer className="footer">
+      <img src={pattern} alt="" />
       <div className="footer-social-area">
         <div>
           <img src={logo} alt="" />
@@ -17,21 +23,23 @@ export const Footer = () => {
           <Typography element="p" variant="paragraph">
             NOS SIGA NAS REDES
           </Typography>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+          <div className="social-icons">
+            <img src={whats} alt="" />
+            <img src={insta} alt="" />
+            <img src={face} alt="" />
           </div>
         </div>
       </div>
-      <div>
+
+      <span className="line"></span>
+
+      <div className="footer-copy-area">
         <div>
           <Typography element="p" variant="paragraph">
-            &copy Copyright 2021 - Elétrica J. Santos - Todos os Direitos
-            Reservados
+            © Copyright 2021 - Elétrica J. Santos - Todos os Direitos Reservados
           </Typography>
         </div>
-        <img src="" alt="" />
+        <img src={logoNoWeb} alt="" style={{ cursor: "pointer" }} />
       </div>
     </footer>
   );
